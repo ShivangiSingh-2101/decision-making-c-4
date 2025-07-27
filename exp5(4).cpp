2f3501a90814746b5a13ec1dc22d355fed4eb716
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 01:56:47 +0530

    Create exp5(4).cpp

---
+++ exp5(4).cpp
@@ -0,0 +1,41 @@
+//Shivangi Singh
+//prn 24070123148
+//B3
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b ,c ;//variables
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Calculator Menu" <<"\n"; 
+    cout << "Enter 1 for Addition"<< "\n";
+    cout << "Enter 2 for Subtraction" << "\n";
+    cout << "Enter 3 for Multiplication" << "\n";
+    cout << "Enter 4 for Division" << "\n";
+    cin >> c;
+    switch(c) { //switch case
+        case 1:
+            cout << "Result: " << a + b ;
+            break;
+        case 2:
+            cout << "Result: " << a - b ;
+            break;
+        case 3:
+            cout << "Result: " << a * b ;
+            break;
+        case 4:
+            if (b != 0) {
+                cout << "Result: " << a/ b;
+            } else {    
+                cout << "not defined" << "\n";
+            }
+            break;
+        default: //default statement
+            cout << "Enter Valid option";
+    }
+    return 0;
+}
+
